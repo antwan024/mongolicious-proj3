3da4b14f105b14c9e46d61b6a7edc220c4842b3f
a.nguyen525i@gmail.com <a.nguyen525i@gmail.com>
2019-11-23 01:40:31 -0700

    went back on server.js

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'server.js'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
