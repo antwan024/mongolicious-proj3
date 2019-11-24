02d8aa6b583042f708853012a292be4a0ff92507
a.nguyen525i@gmail.com <a.nguyen525i@gmail.com>
2019-11-24 15:10:03 -0700

    heroku attempt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB||"mongodb://localhost/bestLife");
-mongoose.connect(process.env.MONGODB|| "mongodb://antwan024:mlab420@ds013946.mlab.com:13946/heroku_h9rvl4jg");
+mongoose.connect(process.env.MONGODB||"mongodb://localhost/bestLife");
+// mongoose.connect(process.env.MONGODB|| "mongodb://antwan024:mlab420@ds013946.mlab.com:13946/heroku_h9rvl4jg");
 
 // Start the API server
 app.listen(PORT, function() {
